35dd28fff03d2d81d1007f577392d6835c14e277
José Gomes <zemiguelgomes99@gmail.com>
2019-04-07 17:31:43 +0100

    juntei tudo e meti uma mini interface

--- projA/MyInterface.js
+++ projA/MyInterface.js
@@ -3,19 +3,26 @@
 * @constructor
 */
 class MyInterface extends CGFinterface {
-    constructor() {
-        super();
-    }
+	constructor() {
+		super();
+	}
 
-    init(application) {
-        // call CGFinterface init
-        super.init(application);
-        // init GUI. For more information on the methods, check:
-        // http://workshop.chromeexperiments.com/examples/gui
-        this.gui = new dat.GUI();
-        
-        var obj = this;
+	init(application) {
+		// call CGFinterface init
+		super.init(application);
+		// init GUI. For more information on the methods, check:
+		// http://workshop.chromeexperiments.com/examples/gui
+		this.gui = new dat.GUI();
 
-        return true;
-    }
+		var obj = this;
+
+		this.gui.add(this.scene, 'displayAxis').name('Display axis');
+
+		this.gui.add(this.scene, 'displayHouse').name('Display House');
+        this.gui.add(this.scene, 'displayTree').name('Display Tree');
+        this.gui.add(this.scene, 'displayPyr').name('Display Pyr');
+
+
+		return true;
+	}
 }
