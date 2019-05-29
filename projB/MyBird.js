397079656168b907201361f789b99b9420b7cc3a
up201706908 <up201706908@fe.up.pt>
2019-05-29 02:09:44 +0100

    Finished bird. Wings and bird's oscillatory movement implemented

--- projB/MyBird.js
+++ projB/MyBird.js
@@ -13,13 +13,17 @@ class MyBird extends CGFobject {
         this.y = y;
         this.z = z;
         this.speed = speed;
+        this.wingAngle = 0;
 	}
 	initBuffers(scene) {
 		scene.cylinder = new MyCylinder(scene,5);
-		scene.pyramid = new MyPyramid(scene, 5, 2);
         scene.plane = new MyQuad (scene);
         scene.cone = new MyCone (scene, 5,0.2);
-        //triangulo
+        scene.plane = new MyQuad (scene); //asa
+        scene.cone = new MyCone (scene, 5,0.2); //lados do corpo e cabeça
+        scene.beak = new MyCone (scene, 5, 0.5); //bico
+        scene.eye = new MyUnitCubeQuad (scene); //eyes 
+        scene.triangle = new MyTriangle (scene); //asas e cauda
     }
     
     display() {
@@ -29,19 +33,24 @@ class MyBird extends CGFobject {
         this.scene.translate(-this.x, -this.y, -this.z);
 
         this.scene.translate(this.x,this.y, this.z);;
+
+        //para começar com orientação 0º
+        this.scene.rotate(90*this.scene.degreeToRad, 0,1,0);
         //corpo
         this.scene.pushMatrix();
+        this.scene.translate (-0.5,0,0);
         this.scene.rotate(-90*this.scene.degreeToRad, 0,0,1);
         this.scene.cylinder.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate (1,0,0);
+        this.scene.translate (0.5,0,0);
         this.scene.rotate(-90*this.scene.degreeToRad, 0,0,1);
         this.scene.cone.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
+        this.scene.translate (-0.5,0,0);
         this.scene.rotate(72.5/2.0*this.scene.degreeToRad, 1,0,0);
         this.scene.rotate(90*this.scene.degreeToRad, 0,0,1);
         this.scene.cone.display();
@@ -50,38 +59,162 @@ class MyBird extends CGFobject {
 
         //cabeca
         this.scene.pushMatrix();
-        this.scene.translate(0.3,0.8,0.8);
-        this.scene.scale(0.5,0.5,0.5);
+        this.scene.translate(-0.35,0.8,0.8);
+        this.scene.scale(0.69,0.7,0.7);
         this.scene.rotate(-90*this.scene.degreeToRad, 0,0,1);
         this.scene.cylinder.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate (0.8,0.8,0.8);
-        this.scene.scale(0.5,0.5,0.5);
+        this.scene.translate (0.34,0.8,0.8);
+        this.scene.scale(0.7,0.7,0.7);
         this.scene.rotate(-90*this.scene.degreeToRad, 0,0,1);
         this.scene.cone.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate(0.3,0.8,0.8);
-        this.scene.scale(0.5,0.5,0.5);
+        this.scene.translate(-0.35,0.8,0.8);
+        this.scene.scale(0.7,0.7,0.7);
         this.scene.rotate(72.5/2.0*this.scene.degreeToRad, 1,0,0);
         this.scene.rotate(90*this.scene.degreeToRad, 0,0,1);
         this.scene.cone.display();
         this.scene.popMatrix();
 
-        this.scene.popMatrix()//MOVIMENTO
+        //bico
+        this.scene.pushMatrix();
+        this.scene.translate(-0.01,0.8,1.34);
+        this.scene.rotate(20*this.scene.degreeToRad, 0,0,1);
+        this.scene.rotate(-10*this.scene.degreeToRad, 1,0,0);
+        this.scene.scale(0.3,0.3,0.9);
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.beak.display();
+        this.scene.popMatrix();
 
-		//cubo
-	/*	this.wallTexture.apply();
-		this.scene.pushMatrix();
-		this.scene.translate(0, 0.5, 0);
-		this.scene.scale(2, 1, 1.3);
-		this.scene.unitCube.display();
-        this.scene.popMatrix();*/
-       // console.log(this.x,this.y,this.z);
-       // console.log(this.orientation);
+        //eyes
+        this.scene.pushMatrix();
+        this.scene.translate(-0.22,1.1,1.35);
+        this.scene.scale(0.2,0.2,0.2);
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.eye.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.21,1.1,1.35);
+        this.scene.scale(0.2,0.2,0.2);
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.eye.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(this.wingAngle/2.0, 0,0,1);
+        //asa direita
+        this.scene.pushMatrix();
+        this.scene.translate(1.0,0.2,0);
+        this.scene.rotate(15*this.scene.degreeToRad, 0,0,1);
+        this.scene.scale(0.8,0.8,0.8);
+        this.scene.scale(1,1,1);
+
+        this.scene.pushMatrix();
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.plane.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-90*this.scene.degreeToRad, 1,0,0);
+        this.scene.plane.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+
+        //triangulos asa direita
+        this.scene.pushMatrix();
+        this.scene.translate(1.77,0.2,0);
+        this.scene.rotate(-15*this.scene.degreeToRad, 0,0,1);
+        this.scene.scale(0.4,1,0.4);
+
+        this.scene.pushMatrix();
+        this.scene.rotate(90*this.scene.degreeToRad, 0,1,0);
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-90*this.scene.degreeToRad, 1,0,0);
+        this.scene.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+        this.scene.popMatrix(); //MOVIMENTO ASA DIREITA
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-this.wingAngle/2.0, 0,0,1);
+
+        //asa esquerda
+        this.scene.pushMatrix();
+        this.scene.translate(-1.0,0.2,0);
+        this.scene.rotate(-15*this.scene.degreeToRad, 0,0,1);
+        this.scene.scale(0.8,0.8,0.8);
+        this.scene.scale(1,1,1);
+
+        this.scene.pushMatrix();
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.plane.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-90*this.scene.degreeToRad, 1,0,0);
+        this.scene.plane.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+
+        //triangulos asa esquerda
+        this.scene.pushMatrix();
+        this.scene.translate(-1.77,0.2,0);
+        this.scene.rotate(15*this.scene.degreeToRad, 0,0,1);
+        this.scene.scale(0.4,1,0.4);
+        this.scene.rotate(90*this.scene.degreeToRad, 0,1,0);
+
+        this.scene.pushMatrix();
+        this.scene.rotate(90*this.scene.degreeToRad, 0,1,0);
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-90*this.scene.degreeToRad, 1,0,0);
+        this.scene.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+        this.scene.popMatrix(); //MOVIMENTO ASA ESQUERDA
+
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-this.wingAngle/5.0, 1,0,0);
+        //cauda
+        this.scene.pushMatrix();
+        this.scene.translate(0,0.4,-1.5);
+        this.scene.rotate(15*this.scene.degreeToRad, 1,0,0);
+        this.scene.scale(0.4,1,1.1);
+        this.scene.rotate(45*this.scene.degreeToRad, 0,1,0);
+
+        this.scene.pushMatrix();
+        this.scene.rotate(90*this.scene.degreeToRad, 0,1,0);
+        this.scene.rotate(90*this.scene.degreeToRad, 1,0,0);
+        this.scene.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-90*this.scene.degreeToRad, 1,0,0);
+        this.scene.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+        this.scene.popMatrix(); //MOVIMENTO CAUDA
+
+    
+        this.scene.popMatrix()//MOVIMENTO GERAL
 	}
 
 
